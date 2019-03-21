c9b081a40fef4f0fa5b55e5a3417ed2a54f77da3
markgmz92 <markgmz92@gmail.com>
2019-03-21 17:27:49 +0800

    Added devise

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+	get 'home/about'
+	get 'home/lookup'
+	post "/home/lookup" => 'home/lookup'
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
