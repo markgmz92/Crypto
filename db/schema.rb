fff59490a6cf779407775e14c893148c26f7d94c
markgmz92 <markgmz92@gmail.com>
2019-03-27 17:01:14 +0800

    Added firstname and lastname to user sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_122443) do
+ActiveRecord::Schema.define(version: 2019_03_27_083448) do
 
   create_table "coins", force: :cascade do |t|
     t.string "symbol"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_03_21_122443) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
